0ece69331f3f80a732bac792ed8f706d99b09e39
Gavin Troy <gavin@cantlogout.com>
2023-02-22 21:39:13 +0000

    Add integrity to CDN resources (+ bump versions)

--- src/plugins.rs
+++ src/plugins.rs
@@ -13,16 +13,10 @@ pub fn new<'r>(plugins: Vec<Box<dyn plugin::Plugin<'r>>>) -> plugin::PluginManag
     );
 
     let base_css_imports = vec![
-        "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",
         "/static/custom.css",
     ];
 
     let base_js_imports = vec![
-        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js",
         "/static/custom.js",
     ];
 
